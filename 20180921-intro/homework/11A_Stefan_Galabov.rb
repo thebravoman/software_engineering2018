4e8e3f8617f915832aaa9fe8306eec33b4ed585b
mamutite <sgalabov22@gmail.com>
2018-09-24 12:51:06 +0300

    Stefan Galabov

---
+++ 20180921-intro/homework/11A_Stefan_Galabov.rb
@@ -0,0 +1,20 @@
+a = ARGV[0].to_f
+b = ARGV[1].to_f
+c = ARGV[2].to_f
+
+discriminant = b*b - 4*a*c
+
+if a == 0
+	puts "a is 0 than the equation in not quadratic."
+	var = -c/b
+	puts "Result : #{var}" 	
+end
+ 
+if discriminant < 0
+	puts "Discriminant less than 0." 
+else
+	x1 = (-b + Math.sqrt(discriminant))/(2*a)
+	x2 = (-b - Math.sqrt(discriminant))/(2*a)
+	puts "X1: #{x1}"
+	puts "X2: #{x2}"
+end
