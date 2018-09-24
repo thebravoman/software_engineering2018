8ac234cf71f8e0ef52ddec36ac121f9c45b5fc75
mamutite <sgalabov22@gmail.com>
2018-09-24 18:21:01 +0300

    Homework updated

--- 20180921-intro/homework/11A_Stefan_Galabov.rb
+++ 20180921-intro/homework/11A_Stefan_Galabov.rb
@@ -3,16 +3,17 @@ b = ARGV[1].to_f
 c = ARGV[2].to_f
 
 discriminant = b*b - 4*a*c
-
+flag = 0
 if a == 0
-	puts "a is 0 than the equation in not quadratic."
+	puts "a is 0 then the equation in not quadratic."
 	var = -c/b
-	puts "Result : #{var}" 	
+	puts "Result : #{var}"
+	flag = 1 	
 end
  
 if discriminant < 0
 	puts "Discriminant less than 0." 
-else
+elsif flag != 1
 	x1 = (-b + Math.sqrt(discriminant))/(2*a)
 	x2 = (-b - Math.sqrt(discriminant))/(2*a)
 	puts "X1: #{x1}"
