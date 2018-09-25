5536588fa35ae4bd1492553b678c908d4ef137a8
TsenkoTsenkov <tseni.tsnekov@gmail.com>
2018-09-25 16:16:51 +0300

    small update

--- 20180921-intro/homework/11A_Tsenko_Tsenkov.rb
+++ 20180921-intro/homework/11A_Tsenko_Tsenkov.rb
@@ -1,11 +1,13 @@
 def quadratic_eq(a, b, c)
-  if b**2 - (4 * a * c) < 0
+  discriminant = b**2 - (4 * a * c) 
+
+  if discriminant < 0
       puts "Not existing roots"
-  elsif b**2 - (4 * a * c) == 0
+  elsif discriminant == 0
          puts "One root x = #{-b/(2 * a)}"
   else 
-        puts "x1 = #{(-b + Math.sqrt(b**2 - (4 * a * c)))/(2 * a)}"
-        puts "x2 = #{(-b - Math.sqrt(b**2 - (4 * a * c)))/(2 * a)}"
+        puts "x1 = #{(-b + Math.sqrt(discriminant))/(2 * a)}"
+        puts "x2 = #{(-b - Math.sqrt(discriminant))/(2 * a)}"
   end
 end
 
