f5c979f82b8a7037f998b0a17a35e42c6c37212c
Kristian-A <kristian.zh.apostolov@gmail.com>
2018-09-25 09:16:29 +0300

    homework
    first commit was wrong file

--- 20180921-intro/homework/11a_kristian_apostolov.rb
+++ 20180921-intro/homework/11a_kristian_apostolov.rb
@@ -1,8 +1,21 @@
-a = ARGV[0].to_i
-b = ARGV[1].to_i
-c = ARGV[2].to_i
+a = ARGV[0].to_f
+b = ARGV[1].to_f
+c = ARGV[2].to_f
 
-x1 = (-b + Math.sqrt(b*b - 4*a*c))/2*a
-x2 = (-b - Math.sqrt(b*b - 4*a*c))/2*a
+if a == 0
+  x = -c/b
+  puts("x = #{x}")
+else
+  d = b*b - 4*a*c
 
-puts("x1: #{x1}, x2: #{x2}");
+  if d < 0
+    d *= -1
+    x1 = "(#{-b} + #{Math.sqrt(d).round(2)}*i)/#{2*a}"
+    x2 = "(#{-b} - #{Math.sqrt(d).round(2)}*i)/#{2*a}"
+    puts("x1 = #{x1}\nx2 = #{x2}")
+  else
+    x1 = ((-b + Math.sqrt(d))/2*a).round(2).to_s
+    x2 = ((-b - Math.sqrt(d))/2*a).round(2).to_s
+    puts("x1 = #{x1}\nx2 = #{x2}")
+  end
+end
