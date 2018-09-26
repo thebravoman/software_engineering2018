1fa2c0838cc8eb6941c98754b19350f31ac65fd4
Antonio Takev <tonitakev@gmail.com>
2018-09-26 22:23:16 +0300

    Checking new cases

--- 20180926-intro-b/11b_antonio_takev.rb
+++ 20180926-intro-b/11b_antonio_takev.rb
@@ -6,17 +6,31 @@ c = ARGV[2].to_i;
 d = b*b - 4*a*c;
 
 
-if d>0
-	x1 = (-b + Math.sqrt(d))/2*a;
-	x2 = (-b - Math.sqrt(d))/2*a;
+unless a == 0
+	if d>0
+		x1 = (-b + Math.sqrt(d))/(2*a);
+		x2 = (-b - Math.sqrt(d))/(2*a);
 
-	puts "x1: #{x1}"
-	puts "x2: #{x2}"
+		puts "x1: #{x1}, #{x2}"
+		
 
-elsif d==0
-	x1 = (-b)/2*a;
-	puts "x1=x2: #{x1.to_i}"
+	elsif d==0
+		x1 = (-b)/(2*a);
+		puts "x1=x2: #{x1}"
 	
-elsif d < 0
-	puts "there aren't real root!" 
+	elsif d < 0
+		puts "There Aren't real roots, which are a solution of the equation!" 
+	end
+
+else
+	unless b == 0
+		x = -c/b;
+		puts "x: #{x}"
+	else
+		unless c == 0
+			puts "There Isn't a solution for this equation!"
+		else
+			puts "Each x is a solution of the equation "
+		end
+	end
 end
