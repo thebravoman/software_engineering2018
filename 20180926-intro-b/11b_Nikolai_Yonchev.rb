9298c4d0becc23d15d5222a8291ebca3e11559b2
Nikolay Yonchev <n.ionchev@gmail.com>
2018-09-26 09:07:01 +0300

    My Second commit

--- 20180926-intro-b/11b_Nikolai_Yonchev.rb
+++ 20180926-intro-b/11b_Nikolai_Yonchev.rb
@@ -3,8 +3,16 @@ a = ARGV[0]
 b = ARGV[1]
 c = ARGV[2]
 
-x1 = (-b + Math.sqrt(b*b - 4*a*c))/2*a
-x2 = (-b - Math.sqrt(b*b - 4*a*c))/2*a
+d = b*b - 4*a*c
 
-puts "X1 : #{x1}"
-puts "X2 : #{x2}"
+if d > 0 
+	x1 = (-b + Math.sqrt(d))/2*a	
+	x2 = (-b - Math.sqrt(d))/2*a
+	puts "X1 : #{x1}"
+	puts "X2 : #{x2}"
+elsif d = 0
+	x = -b/2*a
+	puts "X : #{x}"
+else
+	puts "OPS!"
+end
