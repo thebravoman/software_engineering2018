bb76668d748f5e385f20cb2ae0f28b8d13a24166
papavlov <pavlovplamen01@gmail.com>
2018-09-26 09:09:07 +0300

    Added a func get_roots for better code

--- 20180926-intro-b/11b_Denis_Barzanov.rb
+++ 20180926-intro-b/11b_Denis_Barzanov.rb
@@ -1,24 +1,27 @@
 # ax^2 + bx + c = 0
 
-if (ARGV.length != 3) {
+if (ARGV.length != 3) 
 	abort("Usage: #{$PROGRAM_NAME} <number> <number> <number>")
-}
+end
 
 a = ARGV[0].to_i
 b = ARGV[1].to_i
 c = ARGV[2].to_i
 
-d = b ** 2 - (4 * a * c)
+def get_roots(a, b, c)
 
-if d < 0 
-	abort("Nope")
+	d = b ** 2 - (4 * a * c)
+
+	if d < 0 
+		abort("Nope")
+	end
+
+
+	x1 = (-b + Math.sqrt(d) )/ (2 * a)
+	x2 = (-b - Math.sqrt(d) )/ (2 * a)
+	return x1, x2
 end
 
-x1 = (-b + Math.sqrt(d))/ 2 * a
-x2 = (-b - Math.sqrt(d))/ 2 * a
+x1, x2 = get_roots(a, b, c)
 
-x11 = (-b + Math.sqrt(d) )/ (2 * a)
-x22 = (-b - Math.sqrt(d) )/ (2 * a)
-
-puts "No parentheses : x1 = #{x1} | x2 = #{x2}"
-puts "   Parentheses : x1 = #{x11} | x2 = #{x22}"
+puts "Roots: x1 = #{x1} | x2 = #{x2}"
