470659a7998a21d0ab7879a653ddcca6a905f171
papavlov <pavlovplamen01@gmail.com>
2018-09-26 09:21:36 +0300

    Finished

--- 20180926-intro-b/11b_Denis_Barzanov.rb
+++ 20180926-intro-b/11b_Denis_Barzanov.rb
@@ -8,20 +8,25 @@ a = ARGV[0].to_i
 b = ARGV[1].to_i
 c = ARGV[2].to_i
 
+if (a == 0)
+	abort("Not a quadratic equation...")
+end
+
 def get_roots(a, b, c)
 
 	d = b ** 2 - (4 * a * c)
 
 	if d < 0 
-		abort("Nope")
+		abort("No real roots for the equation")
 	end
 
 
-	x1 = (-b + Math.sqrt(d) )/ (2 * a)
-	x2 = (-b - Math.sqrt(d) )/ (2 * a)
+	x1 = (-b + Math.sqrt(d))/ (2 * a)
+	x2 = (-b - Math.sqrt(d))/ (2 * a)
 	return x1, x2
 end
 
 x1, x2 = get_roots(a, b, c)
 
 puts "Roots: x1 = #{x1} | x2 = #{x2}"
+
