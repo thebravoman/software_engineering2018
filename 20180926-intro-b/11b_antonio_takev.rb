0f1bcad5eb2c3641f069efd3bb4e9915da39d312
Antonio Takev <tonitakev@gmail.com>
2018-09-26 09:20:26 +0300

    checking cases

--- 20180926-intro-b/11b_antonio_takev.rb
+++ 20180926-intro-b/11b_antonio_takev.rb
@@ -1,3 +1,22 @@
 
 a = ARGV[0].to_i;
+b = ARGV[1].to_i;
+c = ARGV[2].to_i;
 
+d = b*b - 4*a*c;
+
+
+if d>0
+	x1 = (-b + Math.sqrt(d))/2*a;
+	x2 = (-b - Math.sqrt(d))/2*a;
+
+	puts "x1: #{x1}"
+	puts "x2: #{x2}"
+
+elsif d==0
+	x1 = (-b)/2*a;
+	puts "x1=x2: #{x1.to_i}"
+	
+elsif d < 0
+	puts "there aren't real root!" 
+end
