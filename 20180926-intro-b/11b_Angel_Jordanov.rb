1b17c696ae2cb641eed0053e39a678ae5786387b
Angel Jordanov <jordanov.a4o@gmail.com>
2018-09-26 09:01:46 +0300

    zdr

--- 20180926-intro-b/11b_Angel_Jordanov.rb
+++ 20180926-intro-b/11b_Angel_Jordanov.rb
@@ -9,9 +9,10 @@ D = B ** 2 - 4*A*C
 if (D == 0)
    puts 'x = '+(-B/2/A).to_s
 else
-   if (D > 0)
+    if (D > 0)
       puts 'x1 = '+((-B-Math.sqrt(D))/2*A).to_s
       puts 'x2 = '+((-B+Math.sqrt(D))/2*A).to_s
 	else 
-		puts 'Nqma realni koreni sori motori...'
+	  puts 'Nqma realni koreni sori motori...'
+	end
 end
