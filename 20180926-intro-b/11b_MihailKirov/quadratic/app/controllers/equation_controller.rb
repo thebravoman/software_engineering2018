d99989f1fe6bfb83a075a0a06de62382f10f6fc2
shimonchick <shimonchick@gmail.com>
2018-09-28 20:08:27 +0300

    made a simple web app solving quadratic equation

--- 20180926-intro-b/11b_MihailKirov/quadratic/app/controllers/equation_controller.rb
+++ 20180926-intro-b/11b_MihailKirov/quadratic/app/controllers/equation_controller.rb
@@ -1,2 +1,51 @@
 class EquationController < ApplicationController
+
+    def solve_equation(a, b, c)
+        output = []
+        if a == 0
+            output.push("not a quadratic equation")
+        else
+            determinant = b*b - 4*a*c;
+            if determinant < 0
+                output.push("no solutions in the real number plane")
+            else
+                x1 = (-b - Math.sqrt(determinant)) / 2*a
+                x2 = (-b + Math.sqrt(determinant)) / 2*a
+                output.push(x1)
+                output.push(x2)
+            end
+        end
+        return output
+    end
+
+    def create
+        #render plain: params[:equation].inspect
+        equation = params[:equation]
+        @a = equation[:a].to_f
+        @b = equation[:b].to_f
+        @c = equation[:c].to_f
+        #@output = solve_equation(@a, @b, @c)
+        @output = []
+        if @a == 0
+            @output.push("Not a quadratic equation")
+        else
+            #puts "#{@a}, #{@b}, #{@c}\n" * 100
+            determinant = @b * @b - 4 * @a * @c
+            if determinant < 0
+                @output.push("No solution in real number plane")
+            elsif determinant == 0
+                x1 = -@b / 2 * @a
+                @output.push(x1)
+            else
+                x1 = (-@b + Math.sqrt(determinant)) / 2 * @a
+                x2 = (-@b - Math.sqrt(determinant)) / 2 * @a
+                @output.push(x1)
+                @output.push(x2)
+            end
+        end
+        render 'result'
+    end
+
+    
+
 end
