a58df4cc7d2fc5b2a484123f869c02a8e3957f84
Martin_Jordanov <martistj@gmail.com>
2018-09-29 14:27:26 +0300

    [Testing] Working script

---
+++ 20180926-quadratic1/homeworks/test_your_homework.rb
@@ -0,0 +1,17 @@
+testing = ARGV[0]
+trusted = ARGV[1]
+100.times do
+	a = rand(20)
+	b = rand(20)
+	c = rand(20)
+	trusted_output = `ruby #{trusted} #{a} #{b} #{c}`
+	testing_output = `ruby #{testing} #{a} #{b} #{c}`
+
+	if trusted_output != testing_output
+		puts "One of the programs gives different output..."
+		puts "Your: #{testing_output}"
+		puts "Trusted: #{trusted_output}"
+		puts "When a=#{a} b=#{b} c=#{c}"
+		return
+	end
+end
