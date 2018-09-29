862cec7ce5f019bface34131d68bee0131821643
shimonchick <shimonchick@gmail.com>
2018-09-29 15:17:00 +0300

    homework 2

--- 20180926-intro-b/11b_MihailKirov/11b_Mihail_Kirov.rb
+++ 20180926-intro-b/11b_MihailKirov/11b_Mihail_Kirov.rb
@@ -1,17 +1,27 @@
 
 def solve_equation(a, b, c)
     if a == 0
-        puts "not a quadratic equation"
-        return [nil, nil]
+        if b == 0
+            if c == 0
+                puts "*"
+            else
+                puts "NaN"
+            end
+        else
+            x = c/b
+            puts "#{x}"
+        end
     else
         determinant = b*b - 4*a*c;
         if determinant < 0
-            puts "no solutions in the real number plane"
-            return [nil, nil]
-        else
+                puts "NaN"
+        elsif determinant == 0
+            x = -b / 2 * a
+            puts "#{x}"
+        else 
             x1 = (-b - Math.sqrt(determinant)) / 2*a
             x2 = (-b + Math.sqrt(determinant)) / 2*a
-            return [x1, x2]
+            puts "#{x1},#{x2}"
         end
     end
 end
@@ -20,8 +30,4 @@ end
 a = ARGV[0].to_i
 b = ARGV[1].to_i
 c = ARGV[2].to_i
-
-x1, x2 = solve_equation(a, b, c)
-if x1 and x2
-    puts "x1: #{x1} x2: #{x2}"
-end
+solve_equation(a, b, c)
