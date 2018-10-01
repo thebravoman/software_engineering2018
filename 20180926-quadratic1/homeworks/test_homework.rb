3a6bac3d49de17cb16c0aedadda0103e34e96b2e
Martin_Jordanov <martistj@gmail.com>
2018-10-01 21:32:49 +0300

    [Formatting] Refactor code
    Signed-off-by: Martin_Jordanov <martistj@gmail.com>

--- 20180926-quadratic1/homeworks/test_homework.rb
+++ 20180926-quadratic1/homeworks/test_homework.rb
@@ -1,23 +1,19 @@
-# Dobavih negativni stoinosti. Predi beshe pochti nevuzmozhno da vlezesh v discriminanta. ~Krasi
-
 testing = ARGV[0]
 trusted = ARGV[1]
 100.times do |i|
-	a = rand(20) - 10
-	b = rand(20) - 10
-	c = rand(20) - 10
-	trusted_output = `ruby #{trusted} #{a} #{b} #{c}`
-	testing_output = `ruby #{testing} #{a} #{b} #{c}`
-	
-	if i % 10 == 0
-		puts "#{i}/100 test cases"
-	end
-	if trusted_output != testing_output
-		puts "One of the programs gives different output..."
-		puts "Your: #{testing_output}"
-		puts "Trusted: #{trusted_output}"
-		puts "When a=#{a} b=#{b} c=#{c}"
-		return
-	end
+  a = rand(-10..9)
+  b = rand(-10..9)
+  c = rand(-10..9)
+  trusted_output = `ruby #{trusted} #{a} #{b} #{c}`
+  testing_output = `ruby #{testing} #{a} #{b} #{c}`
+
+  puts "#{i}/100 test cases" if i % 10.zero?
+  next unless trusted_output != testing_output
+
+  puts 'One of the programs gives different output...'
+  puts "Your: #{testing_output}"
+  puts "Trusted: #{trusted_output}"
+  puts "When a=#{a} b=#{b} c=#{c}"
+  return
 end
 puts "After 100 different test cases there's no difference"
