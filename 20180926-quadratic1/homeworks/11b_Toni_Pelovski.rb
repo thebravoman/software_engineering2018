086bc772bec0ba8577730e85bf9d8ca71f9d6c1e
root <tonipelovski@gmail.com>
2018-10-01 13:49:21 +0300

    Homework

---
+++ 20180926-quadratic1/homeworks/11b_Toni_Pelovski.rb
@@ -0,0 +1,29 @@
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+c = ARGV[2].to_i
+
+D = b*b - 4*a*c
+
+if D < 0 
+    puts "NaN"
+elsif a == 0
+    if b == 0
+        if c == 0
+	    puts "*"
+	else
+	    puts "NaN"
+	    end
+    else
+	x = -c/b
+	puts "#{x.round(3)}"
+        end
+else
+    x1 = (-b + Math.sqrt(D)) / (2*a)
+    x2 = (-b - Math.sqrt(D)) / (2*a)
+    if x1 != x2
+	    puts "#{x1.round(3)},#{x2.round(3)}"
+    else
+	    puts "#{x1.round(3)}"
+	    end
+    end
+
