dc5cc33920587e7bd41ad9420ee4592dc331247c
root <tonipelovski@gmail.com>
2018-10-01 14:03:36 +0300

    Fixes

--- 20180926-quadratic1/homeworks/11b_Toni_Pelovski.rb
+++ 20180926-quadratic1/homeworks/11b_Toni_Pelovski.rb
@@ -1,6 +1,6 @@
-a = ARGV[0].to_i
-b = ARGV[1].to_i
-c = ARGV[2].to_i
+a = ARGV[0].to_f
+b = ARGV[1].to_f
+c = ARGV[2].to_f
 
 D = b*b - 4*a*c
 
@@ -18,8 +18,8 @@ elsif a == 0
 	puts "#{x.round(3)}"
         end
 else
-    x1 = (-b + Math.sqrt(D)) / (2*a)
-    x2 = (-b - Math.sqrt(D)) / (2*a)
+    x1 = (-b - Math.sqrt(D)) / (2*a)
+    x2 = (-b + Math.sqrt(D)) / (2*a)
     if x1 != x2
 	    puts "#{x1.round(3)},#{x2.round(3)}"
     else
