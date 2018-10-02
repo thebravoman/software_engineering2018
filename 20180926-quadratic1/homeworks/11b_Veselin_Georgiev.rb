c218bd9712f7bee7c71602829c44dc15c62ad5bc
Martin_Jordanov <martistj@gmail.com>
2018-10-03 00:01:01 +0300

    Update

--- 20180926-quadratic1/homeworks/11b_Veselin_Georgiev.rb
+++ 20180926-quadratic1/homeworks/11b_Veselin_Georgiev.rb
@@ -5,7 +5,7 @@ c = ARGV[2].to_f
 D = b*b - 4*a*c
 
 if D < 0 
-    puts "There are no real solutions to this equation. (D < 0)"
+    puts "NaN"
 elsif a == 0
     if b == 0
         if c == 0
@@ -18,8 +18,8 @@ elsif a == 0
 	puts "#{x}"
         end
 else
-    x1 = ((-b + Math.sqrt(D)) / 2*a).round(3)
-    x2 = ((-b - Math.sqrt(D)) / 2*a).round(3)
+    x1 = ((-b + Math.sqrt(D)) / (2*a)).round(3)
+    x2 = ((-b - Math.sqrt(D)) / (2*a)).round(3)
 
     if x2 > x1
     puts "#{x1},#{x2}"
