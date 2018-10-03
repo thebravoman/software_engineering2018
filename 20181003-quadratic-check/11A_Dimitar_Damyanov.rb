ddaf00c08ef4562e8f872899e7bcc822f221b4a2
dDamyanov0305 <dimitar.damyanov.0305@gmail.com>
2018-10-03 21:52:23 +0300

    vsi4ki g1rmqt

--- 20181003-quadratic-check/11A_Dimitar_Damyanov.rb
+++ 20181003-quadratic-check/11A_Dimitar_Damyanov.rb
@@ -1,45 +1,59 @@
+content = File.read("allfiles.txt")
+content = content.split("\n")
+file_to_write=File.open("results.csv","w")
 
+l=
+[
+    [0,0,0],
+    [0,0,5],
+    [0,5,0],
+    [5,0,0],
+    [0,3,4],
+    [4,0,5],
+    [4,0,-5],
+    [3,4,0],
+    [4,4,1],
+    [2,3,4],
+    [1,3,2]
+]
 
- content = File.read("allfiles.txt")
-    content = content.split("\n")
+res=
+[
+    ['*'],
+    ["NaN"],
+    ["0"],
+    ["0"],
+    ["-1.333"],
+    ["NaN"],
+    ["1,118"],
+    ["-1.333,0"],
+    ["0.5"],
+    ["NaN"],
+    ["-2,-1"]
 
+]
     all_programs = content.
             map {|x| x.include?(".rb") ? x : nil }.
             compact.
             map {|x| x.split(" ")[-1]}
 
     all_programs.each do |program|
-            result = `ruby #{program} 0 0 0` #all=0
-            puts (result=="*") ? 1:0
-
-            result = `ruby #{program} 0 4 5` #a=0
-            puts (result=="-1.25") ? 1:0
-
-            result = `ruby #{program} 0 3 4` #3 znaka sled deseti4nata
-            puts (result=="-1.333") ? 1:0
-
-            result = `ruby #{program} 4 0 5` #b=0
-            puts (result=="*") ? 1:0
-
-            result = `ruby #{program} 4 5 0` #c=0
-            puts (result=="*") ? 1:0
-
-            result = `ruby #{program} 0 0 5` 
-            puts (result=="NaN") ? 1:0
-
-            result = `ruby #{program} 0 5 0` 
-            puts (result=="NaN") ? 1:0
-
-            result = `ruby #{program} 5 0 0` 
-            puts (result=="NaN") ? 1:0
-
-            result = `ruby #{program} 4 4 1` #D=0
-            puts (result=="1/2" ? 1:0)
-
-            result = `ruby #{program} 2 3 4` #D<0
-            puts (result=="NaN") ? 1:0
-
-            result = `ruby #{program} 1 3 2` #D>0
-            puts (result=="-2,-1") ? 1:0
+            puts program
+            i=0
+            r=0
+            while i<11
+                result=`ruby ../20180926-quadratic1/homeworks/#{program} #{l[i][0]} #{l[i][1]} #{l[i][2]}`
+                result=result.gsub("\n","")
+                puts result
+                r=result==res[i][0]? 1:0
+                if r==0
+                    break
+                end
+                i+=1
+            end
+            puts r
+            name_=program.gsub(".rb","").split("_").join(",")
+            file_to_write.write(name_ + " :" + r.to_s+"\n")
             
     end
+    file_to_write.close()
