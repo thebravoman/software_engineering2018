c4e45e014dcab3ec512e1f05f78e156fe8771d30
NickSot <nikola.s.sotirov@gmail.com>
2018-10-03 15:33:42 +0300

    fixing

--- 20180926-quadratic1/homeworks/11a_Nikola_Sotirov.rb
+++ 20180926-quadratic1/homeworks/11a_Nikola_Sotirov.rb
@@ -3,16 +3,36 @@ b = Float(ARGV[1])
 c = Float(ARGV[2])
 
 def solve_quad_eq(a, b ,c)
-		if (a == 0)
-			if b == 0
-				return "NaN"
-			end
-
+		if a == 0
 			if b == 0 && c == 0
 				return "*"
 			end
+
+			if b == 0
+				return "NaN"
+			end
 			
-			return (-c/b).round(3)
+			result = -c/b
+
+			if result == -0.0
+				result = -result
+			end
+
+			return (result).round(3)
+		else
+			if b == 0 && c == 0
+				return 0.0
+			end
+
+			if b == 0
+				result = -c/a
+
+				if result == -0.0
+					result = 0.0
+				end
+
+				return result
+			end
 		end
 
 		d = b*b - 4*a*c
@@ -22,11 +42,24 @@ def solve_quad_eq(a, b ,c)
 		end
 
 		if d == 0
-			return (-b/(2*a)).round(3)
+			result = -b/(2*a)
+			if (result == -0.0)
+				result = -result
+			end
+
+			return result.round(3)
 		end
 
 		x1 = (-b + Math.sqrt(d))/(2*a)
 		x2 = (-b - Math.sqrt(d))/(2*a)
+
+		if x1 == -0.0
+			x1 = -x1
+		end
+
+		if x2 == -0.0
+			x2 = -x2
+		end
 
 		if x1 > x2
 			x1, x2 = x2, x1
