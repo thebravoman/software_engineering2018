5bd1227d5b809e51a37905894ccf664abcea5fa7
Narkotik <vvladimirov583@gmail.com>
2018-10-04 19:59:06 +0300

    fix for 3 pls

--- 20180926-quadratic1/homeworks/11a_Viktor_Vladimirov.rb
+++ 20180926-quadratic1/homeworks/11a_Viktor_Vladimirov.rb
@@ -7,6 +7,9 @@ if a == 0 && b == 0 && c == 0
     
 elsif a == 0
     x = (-c/b).round(3)
+    if x == -0.0
+		x = 0.0
+	end
     puts "#{x}"
 
 else
@@ -16,10 +19,19 @@ else
         
     elsif d == 0
         x = (-b/(2*a)).round(3)
+        if x == -0.0
+            x = 0.0
+        end
         puts "#{x}"
     else
         x1 = ((- b + Math.sqrt(d))/(2*a)).round(3)
         x2 = ((- b - Math.sqrt(d))/(2*a)).round(3)
+        if x1 == -0.0
+            x1 = 0.0
+        end
+        if x2 == -0.0
+            x2 = 0.0
+        end
         if x1 > x2
             puts "#{x2},#{x1}"
         else
