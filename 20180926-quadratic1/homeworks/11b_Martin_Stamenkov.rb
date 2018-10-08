9b66d7c745a37db73e359e3144c134c6b2358dcc
Martin Stamenkov <bul2013@abv.bg>
2018-10-08 18:46:12 +0300

    Fixed -0.0

--- 20180926-quadratic1/homeworks/11b_Martin_Stamenkov.rb
+++ 20180926-quadratic1/homeworks/11b_Martin_Stamenkov.rb
@@ -7,8 +7,11 @@ c = Float(ARGV[2])
 if(a == 0)
 	if(b!=0)
 		x = (-c/b).round(3);
-		puts(x);
+		if(x == -0.0)
+		puts(0.0);
 		abort();
+		end
+		
 		
 	elsif(c!=0)
 			puts("NaN");
@@ -26,7 +29,9 @@ D = b*b - (4*a*c);
 
 if(D == 0)
 	x = (-b/(2*a)).round(3);
-	puts(x);
+	if(x == -0.0)
+		puts(0.0);
+	end
 end
 if(D < 0)
 	puts("NaN");
