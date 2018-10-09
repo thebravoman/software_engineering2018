45b4da945bbc4c6d595b98d4d54982d9b28986cf
Nikolay Yonchev <n.ionchev@gmail.com>
2018-10-09 22:26:11 +0300

    HW

--- 20180926-quadratic1/homeworks/11b_Nikolai_Yonchev.rb
+++ 20180926-quadratic1/homeworks/11b_Nikolai_Yonchev.rb
@@ -17,20 +17,24 @@ if a == 0
 		puts "#{x.round(3)}"
 	end
 else
-	if d < 0
-		puts "NaN"
-	elsif d == 0
-		x = -b / (2*a)
-		puts "#{x.round(3)}"
+	if b==0 && c==0
+		puts "0.0";
 	else
-		x1 = (-b + Math.sqrt(d)) / (2*a)
-		x2 = (-b - Math.sqrt(d)) / (2*a)
-		if x1 > x2
-			c = x1
-			x1 = x2
-			x2 = c
+		if d < 0
+			puts "NaN"
+		elsif d == 0
+			x = -b / (2*a)
+			puts "#{x.round(3)}"
+		else
+			x1 = (-b + Math.sqrt(d)) / (2*a)
+			x2 = (-b - Math.sqrt(d)) / (2*a)
+			if x1 > x2
+				c = x1
+				x1 = x2
+				x2 = c
+			end
+			puts "#{x1.round(3)},#{x2.round(3)}"
 		end
-		puts "#{x1.round(3)},#{x2.round(3)}"
 	end
 end	
 
