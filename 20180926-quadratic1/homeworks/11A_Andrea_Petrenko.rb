a6520cbbaa2d44c39a02bd47ef66428fa2f395b5
Your Name <adito213@gmail.com>
2018-10-09 07:32:11 +0200

    ~Final~

--- 20180926-quadratic1/homeworks/11A_Andrea_Petrenko.rb
+++ 20180926-quadratic1/homeworks/11A_Andrea_Petrenko.rb
@@ -1,3 +1,7 @@
+a = ARGV[0].to_f
+b = ARGV[1].to_f
+c = ARGV[2].to_f
+
 if(a == 0)
 	if(b == 0 && c == 0)
 		puts"*"
@@ -7,31 +11,31 @@ if(a == 0)
 		x = -c/b
 		if(x == -0.0)
 					x = 0.0
-				end
-		puts "#{x.round(3)}"
 		end
+		puts "#{x.round(3)}"
+	end
 	exit 0
 end
 
-D = b*b - 4*a*c
+d = b*b - 4*a*c
 
-if(D < 0)
+if(d < 0)
 	puts "NaN"
 end
 
-if(D==0)
+if(d == 0)
 	x1 = -b/(2*a)
 	if(x1 == -0.0)
 		x1 = 0.0
 	end
 	puts"#{x1.round(3)}"
-elsif(D>0)
-	x1 = (-b + Math.sqrt(D))/(2*a)
-	x2 = (-b - Math.sqrt(D))/(2*a)
-	if(x1<x2)
-		puts "#{x1.round(3)},#{x2.roundi(3)}"
-		end
-	if(x2<x1)
+elsif(d > 0)
+	x1 = (-b + Math.sqrt(d))/(2*a)
+	x2 = (-b - Math.sqrt(d))/(2*a)
+	if(x1 < x2)
+		puts "#{x1.round(3)},#{x2.round(3)}"
+	end
+	if(x2 < x1)
 		puts "#{x2.round(3)},#{x1.round(3)}"
-		end
+	end
 end
