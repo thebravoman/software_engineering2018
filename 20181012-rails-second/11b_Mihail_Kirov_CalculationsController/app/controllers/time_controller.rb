0aec15d28f9d3bdf72c7260881824c9cf8eeb7ce
shimonchick <shimonchick@gmail.com>
2018-10-18 21:29:13 +0300

    quadratic

--- 20181012-rails-second/11b_Mihail_Kirov_CalculationsController/app/controllers/time_controller.rb
+++ 20181012-rails-second/11b_Mihail_Kirov_CalculationsController/app/controllers/time_controller.rb
@@ -2,7 +2,7 @@ class TimeController < ApplicationController
 
     def new
         time = Time.now
-        render plain: time.inspect
+        render plain: time.inspect + "Number in class: 19"
     end
 
 end
