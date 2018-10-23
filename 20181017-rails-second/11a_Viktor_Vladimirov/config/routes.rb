63dc7f90ab009fdd919e5caeff23659f2ec3d026
Narkotik <vvladimirov583@gmail.com>
2018-10-23 22:20:56 +0300

    pls 3 3
    3 e zlatna ocenka

--- 20181017-rails-second/11a_Viktor_Vladimirov/config/routes.rb
+++ 20181017-rails-second/11a_Viktor_Vladimirov/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
-  post '/calculations/new', to: 'calculations#new', as: "calculations"
-  resources :calculations
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    post "/calculations/create", to: "calculations#create", as: "calculations"
+    resources :calculations
 end
