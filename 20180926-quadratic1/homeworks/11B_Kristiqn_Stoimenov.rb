3d31ddda9964926467e0a805c7c9e91842a514f9
puka89 <ludiat.lud@abv.bg>
2018-10-28 11:16:14 +0300

    HW1

---
+++ 20180926-quadratic1/homeworks/11B_Kristiqn_Stoimenov.rb
@@ -0,0 +1,49 @@
+a = ARGV[0].to_f;
+b = ARGV[1].to_f;
+c = ARGV[2].to_f;
+
+d = b*b - 4*a*c;
+
+
+unless a == 0
+	if d>0
+		x1 = ((-b + Math.sqrt(d))/(2*a)).round(3);
+		x2 = ((-b - Math.sqrt(d))/(2*a)).round(3);
+		if(x1<x2)
+			puts "#{x1},#{x2}"
+		else
+			puts "#{x2},#{x1}"
+		end
+
+	elsif d==0
+		x1 = ((-b)/(2*a)).round(3);
+		if x1 == -0.0
+			x12 = ((b)/(2*a)).round(3);		
+			puts "#{x12}"
+		else
+			
+			puts "#{x1}"
+		end
+	elsif d < 0
+		puts "NaN" 
+	end
+
+else
+	unless b == 0
+		x = (-c/b).round(3);
+		if x == -0.0
+			x1 = (c/b).round(3); 		
+			puts "#{x1}"
+		else
+			
+			puts "#{x}"
+		end
+		
+	else
+		unless c == 0
+			puts "NaN"
+		else
+			puts "*"
+		end
+	end
+end
