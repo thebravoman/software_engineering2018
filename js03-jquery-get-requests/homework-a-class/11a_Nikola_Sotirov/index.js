47cef776a5a2429042f62dd4a1e87e5347a2d0c6
NickSot <nikola.s.sotirov@gmail.com>
2018-11-04 11:11:08 +0200

    More changes

--- js03-jquery-get-requests/homework-a-class/11a_Nikola_Sotirov/index.js
+++ js03-jquery-get-requests/homework-a-class/11a_Nikola_Sotirov/index.js
@@ -3,6 +3,7 @@ var btn = document.getElementById("btn");
 var container = document.getElementById("container");
 
 var btnClick = () => {
+    container.removeChild(container.childNodes[0]);
 
     var url = document.getElementById("url").value;
 
@@ -14,13 +15,33 @@ var btnClick = () => {
 
     Http.onreadystatechange = () => 
     {
+        var obj = {};
+
         if (Http.status == 200 && url != ""){
-            var res = document.createTextNode(`${Http.responseText}\n`);
+            obj["success"] = true;
+            try{
+                obj["object"] = JSON.parse(Http.responseText);
+            }
+            catch{
+                return;
+            }
+
+            var res = document.createTextNode(`${JSON.stringify(obj, null, 2)}\n`);
 
             container.appendChild(res);
+
+            return;
         }
         else{
-            container.appendChild(document.createTextNode("WRONG URL!!!\n"));
+            obj["success"] = false;
+
+            obj["statusCode"] = Http.status;
+
+            container.appendChild(document.createTextNode(`${JSON.stringify(obj, null, 2)}\n`));
+
+            return;
+
+            //container.appendChild(document.createTextNode("WRONG URL!!!\n"));
         }
     }
 }
