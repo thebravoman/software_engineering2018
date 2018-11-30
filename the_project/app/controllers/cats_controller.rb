0e0bb453c4a296a890d0aae5427fb77c732d178f
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2018-11-30 08:08:55 +0200

    The code for the third lecture on Rails

--- the_project/app/controllers/cats_controller.rb
+++ the_project/app/controllers/cats_controller.rb
@@ -4,8 +4,31 @@ class CatsController < ApplicationController
   end
 
   def create
-    cat = Cat.create(cat_params)
-    redirect_to cat_path(cat)
+    cat = Cat.new(cat_params)
+    if cat.save
+      redirect_to cat_path(cat)
+    else
+      redirect_to new_cat_path
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    cat = Cat.find(params[:id])
+    if cat.update(cat_params)
+      redirect_to cat_path(cat)
+    else
+      redirect_to edit_cat_path(cat)
+    end
+  end
+
+  def destroy
+    cat = Cat.find(params[:id])
+    cat.destroy
+    redirect_to cats_path
   end
 
   def index
