763cb173a4d05ce43b9fab00229d22bf51cd4a95
ALMLAMBO <aleksm007148@gmail.com>
2018-12-05 18:06:50 +0200

    fix of the homework

--- 20181109-node-server/homework/a-class/11a_Aleksander_Marinov.js
+++ 20181109-node-server/homework/a-class/11a_Aleksander_Marinov.js
@@ -5,21 +5,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', function (request, response) {
-  response.json({'studentNr':'01'});
+  response.json({'studentNr': '01'});
 });
 
 app.post('/', function(request, response) {
   console.log(request.body);
-  var body = request.body;
-  if(body['whoami'] === 'id') {
+  if(request.body['whoami'] === 'id') {
     response.json({'I am': '01'});
   }
-  else if(body['action'] === 'id') {
+  else if(request.body['action'] === 'id') {
     response.json(1);
   }
 });
-
-module.exports = app;
 
 app.listen(2001, function () {
   console.log('Example app listening on port 2001!');
