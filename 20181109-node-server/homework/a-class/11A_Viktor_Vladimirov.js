150b3369e23e3dbd81051d132687dae0406632fb
Wolf <vvladimirov583@gmail.com>
2018-12-06 17:44:54 +0200

    uuuuuuuuuuuuuuuuuuuuuuuu

---
+++ 20181109-node-server/homework/a-class/11A_Viktor_Vladimirov.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+app.get('/', function (request, response) {
+  response.json({'studentNr':'5'});
+});
+
+app.post('/', function(request, response){
+      if(request.body['action']=='id')
+        response.json(5);
+      else if(request.body['whoami']=='id')
+        response.json({'I am':'05'});
+});
+
+
+app.listen(2005);
