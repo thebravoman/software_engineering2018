1407c40e373d623722bc45b8ab5b714dc83ad30d
AndreaP <37434468+Adito213@users.noreply.github.com>
2018-12-06 21:51:30 +0200

    Create 11A_Andrea_Petrenko.js

---
+++ 20181109-node-server/homework/a-class/11A_Andrea_Petrenko.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+app.get('/', function (request, response) {
+  response.json({'studentNr':'02'});
+
+});
+
+app.post('/', function(request, response){
+  if(request.body["whoami"] == "id")
+    response.json({'I am' : 'AP'});
+  else if(request.body["action"] == "id")
+    response.json(2);
+  else {
+    response.status(404).json({"NOOOO":"THIS WAS NOT FOUND!"});
+  }
+});
+
+
+app.listen(2002, function () {
+  console.log('Aleluyaa this app runs!!!  It is on port 2002! :) ');
+});
