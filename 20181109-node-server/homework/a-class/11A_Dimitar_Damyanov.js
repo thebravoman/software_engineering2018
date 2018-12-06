c333db2a2ad5778a3bd249ab3d857b3af2484a65
dDamyanov0305 <dimitar.damyanov.0305@gmail.com>
2018-12-06 16:09:29 +0200

    my precious

---
+++ 20181109-node-server/homework/a-class/11A_Dimitar_Damyanov.js
@@ -0,0 +1,37 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app= express();
+
+app.use(bodyParser.json());
+
+app.get("/",(req,res)=>{
+    //res.set("content-type","application/json");
+    res.json({"studentNr" : 9});
+});
+
+app.post("/",(req,res)=>{
+    //res.set("content-type","application/json");
+    body=req.body;
+
+   /*for(let key in body){
+
+        if(key=="action"){
+                res.json(9);
+            
+        }
+        else if(key=="whoami"){
+                res.json({"I am" : 9});
+        }
+        
+    };*/
+    if(body["action"]=="id"){
+        res.json(9);
+    }
+
+    else if(body["whoami"]=="id"){
+        res.json({"I am" : 9});
+    }
+});
+
+app.listen(2009,()=>console.log("server started successfully"));
