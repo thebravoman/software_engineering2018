2b72768210d6af1ff04f5f4b13476268f626666b
titi_2001 <titi_2001@abv.bg>
2018-12-20 22:38:37 +0200

    Homework

---
+++ 20180712-node-modules-and-tests/homework/11A_Konstantin_Georgiev/test.js
@@ -0,0 +1,7 @@
+var date = require("./date.js");
+var day = new Date();
+exports.testToday = (test) => {
+    var result = "Hello, today is " + day.getFullYear() + "-" + parseInt(day.getUTCMonth() + 1) + "-" + day.getDate();
+    test.equals(date.today(), result);
+    test.done();
+}
