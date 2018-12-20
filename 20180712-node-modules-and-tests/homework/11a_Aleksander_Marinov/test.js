0fca0edd7da3b3071330c482ce0c479728bef7eb
ALMLAMBO <aleksm007148@gmail.com>
2018-12-20 17:58:45 +0200

    add mocking

--- 20180712-node-modules-and-tests/homework/11a_Aleksander_Marinov/test.js
+++ 20180712-node-modules-and-tests/homework/11a_Aleksander_Marinov/test.js
@@ -1,9 +1,16 @@
+const sinon = require('sinon');
 let info = require("./date.js");
 let date = new Date();
 
 exports.testToday = function(test) {
     let expected_result = date.getFullYear() + "-" 
     + (date.getMonth() + 1) + "-" + date.getDate();
-    test.equals(info.today(), expected_result);
+
+    let object = {'date' :  function() {}};
+    let mock = sinon.mock(object);
+    mock.expects('date').once().withArgs(test).returns(expected_result);
+    object.date(test);
+    //test.equals(info.today(), expected_result);
+    mock.verify();
     test.done();
 }
