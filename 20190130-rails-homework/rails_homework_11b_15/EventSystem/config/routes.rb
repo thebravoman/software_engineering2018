45c6e1f3ad162c5320766d27cec9725c2b8f10bf
Martin_Jordanov <martistj@gmail.com>
2019-01-19 20:44:56 +0200

    [Update] The test was successful! modeling the join model without need of controller
    Signed-off-by: Martin_Jordanov <martistj@gmail.com>

--- 20190130-rails-homework/rails_homework_11b_15/EventSystem/config/routes.rb
+++ 20190130-rails-homework/rails_homework_11b_15/EventSystem/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :people
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post "/people/:person_id/add_event" => 'people#add_event'
 end
