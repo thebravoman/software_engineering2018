4c0669b5c4f2651651de9d7f95d8630dbb6c0d03
Wolf <vvladimirov583@gmail.com>
2019-01-19 20:29:47 +0200

    Rails homework [RAILS IS DOODOOO]

---
+++ 20190130-rails-homework/rails_homework_11a_05/db/migrate/20190119163525_create_attendances.rb
@@ -0,0 +1,10 @@
+class CreateAttendances < ActiveRecord::Migration[5.2]
+  def change
+    create_table :attendances do |t|
+      t.string :name
+      t.string :event
+
+      t.timestamps
+    end
+  end
+end
