ea4bad4cf10d3119dcb02e2badab8160871b30aa
Martin_Jordanov <martistj@gmail.com>
2019-01-19 18:04:49 +0200

    [Update] Form for selecting events for person

--- 20190130-rails-homework/rails_homework_11b_15/EventSystem/app/models/event.rb
+++ 20190130-rails-homework/rails_homework_11b_15/EventSystem/app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ApplicationRecord
     has_many :people, through: :attendances
     validates :name, length: {minimum:8}
     validates_presence_of :time, message: "can't be blank"
+
 end
