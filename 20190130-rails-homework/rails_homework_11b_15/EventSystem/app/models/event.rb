b9fc383f97e773786000cdf4be535af429985dd7
Martin_Jordanov <martistj@gmail.com>
2019-01-19 16:30:46 +0200

    [Update] Big progress thanks to scaffold

--- 20190130-rails-homework/rails_homework_11b_15/EventSystem/app/models/event.rb
+++ 20190130-rails-homework/rails_homework_11b_15/EventSystem/app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
     has_many :attendances
     has_many :people, through: :attendances
+    validates :name, length: {minimum:8}
+    validates_presence_of :time, message: "can't be blank"
 end
--- 20190130-rails-homework/rails_homework_11b_15/EventSystem/app/models/person.rb
+++ 20190130-rails-homework/rails_homework_11b_15/EventSystem/app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
     has_many :attendances
     has_many :events, through: :attendances
+    validates_presence_of :name, message: "can't be blank"
 end
