0ca35b589fbd2093238ebaa3bd150931a4644498
Martin_Jordanov <martistj@gmail.com>
2019-01-20 23:34:01 +0200

    [Fix] Better routes 'people/:id' => 'person/:id'
    Signed-off-by: Martin_Jordanov <martistj@gmail.com>

--- 20190130-rails-homework/rails_homework_11b_15/EventSystem/config/routes.rb
+++ 20190130-rails-homework/rails_homework_11b_15/EventSystem/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'people#index'
-  post "/people/:id/add_event" => 'people#subscribe_event'
-  delete "/people/:id/unsubscribe/:event_id" => 'people#unsubscribe_event'
+  post "/person/:id/add_event" => 'people#subscribe_event'
+  delete "/person/:id/unsubscribe/:event_id" => 'people#unsubscribe_event'
 end
