8f63f4deedf4c539cfece71d8da1a5a729b784e1
blackcat <stef4oben@abv.bg>
2019-01-22 19:18:43 +0200

    +Added session +Added login/logout/signup *Fixed documents

--- c_january/11B_23_Stefan_Stefanov/test/controllers/main_controller_test.rb
+++ c_january/11B_23_Stefan_Stefanov/test/controllers/main_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class LoginControllerTest < ActionDispatch::IntegrationTest
+class MainControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
