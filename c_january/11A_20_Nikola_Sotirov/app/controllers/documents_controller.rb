4fb8af1bed6145dac30903126b85d63053de7b68
NickSot <nikola.s.sotirov@gmail.com>
2019-01-22 13:13:24 +0200

    Almost finished, struggling with the mailing feature...

---
+++ c_january/11A_20_Nikola_Sotirov/app/controllers/documents_controller.rb
@@ -0,0 +1,52 @@
+class DocumentsController < ApplicationController
+	@@message = ''
+
+	def root
+		
+	end
+
+	def index
+		@msg = @@message
+	end
+
+	def new
+		
+	end
+
+	def show
+		puts session[:email] == params[:user]
+
+		if String(session[:email]) != params[:user] or String(session[:password]) != params[:pass]
+			if params[:user] != nil and params[:pass] != nil
+				@@message = 'You are not permitted to view this document!'
+				redirect_to '/documents/'
+			end
+		end
+
+		@document = Document.find params[:id]
+	end
+
+	def create
+		file_name = params[:document][:file].original_filename
+
+		content = params[:document][:file].read
+
+		ascii_str = content.unpack("U*").map{|c|c.chr}.join
+
+		doc = Document.create name: file_name, text: ascii_str
+
+		user = User.where(email: session[:email], password: session[:password]).take
+
+		user.documents << doc	
+
+		render 'index'	
+	end
+
+	def edit
+		
+	end
+
+	def document_params
+		return params.require(:document).permit(:file)
+	end
+end
---
+++ c_january/11A_20_Nikola_Sotirov/app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+	def index
+
+	end
+
+	def login
+		u = User.where(user_params).take
+
+		if u == nil
+			@message = "Credentials invalid!"
+			render 'index'
+			return
+		else
+			@message = "Successfully logged!"
+		end
+
+		user = User.find u.id
+
+		session[:email] = user.email
+		session[:password] = user.password
+
+		puts session[:email]
+
+		render 'index'
+	end
+
+	def new
+
+	end
+
+	def create
+		User.create user_params
+
+		
+
+		redirect_to '/users/login'
+	end
+
+	def user_params
+		return params.require(:user).permit(:email, :password)
+	end
+end
