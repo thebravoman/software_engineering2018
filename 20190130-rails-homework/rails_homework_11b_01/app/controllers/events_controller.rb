fa2cf3f86a80c11fbd6b1f4b8f0fc8c5b7839fcf
sa6oznaka <treildr@gmail.com>
2019-01-25 17:12:00 +0200

    added edit/remove

--- 20190130-rails-homework/rails_homework_11b_01/app/controllers/events_controller.rb
+++ 20190130-rails-homework/rails_homework_11b_01/app/controllers/events_controller.rb
@@ -24,6 +24,26 @@ class EventsController < ApplicationController
 				
 	end	
 
+	def edit
+		@event = Event.find(params[:id]);
+	end	
+
+	def update
+		@event = Event.find(params[:id]);
+
+		if(@event.update(event_params))
+			redirect_to @event
+		else
+			render 'edit'	
+		end
+	end	
+
+	def destroy
+		@event = Event.find(params[:id]);
+		@event.destroy
+
+		redirect_to events_path		
+	end	
 
 
 	private def event_params
