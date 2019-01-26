83f0ca99943e2e8c64a79543497dd0b4ce85fb06
DenisBarzanov <denis.barzanov2002@gmail.com>
2019-01-26 13:04:29 +0000

    Added homework and tried to fix somebody's force push

---
+++ 20190130-rails-homework/rails_homework_11b_11/db/migrate/20190125120744_create_attendances.rb
@@ -0,0 +1,10 @@
+class CreateAttendances < ActiveRecord::Migration[5.2]
+  def change
+    create_table :attendances do |t|
+      t.belongs_to :person
+      t.belongs_to :event
+
+      t.timestamps
+    end
+  end
+end
